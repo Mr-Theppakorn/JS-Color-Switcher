6c2cdd0907e694b0674c0cbeadcc80298cd80469
Mr-Theppakorn <38773611+Mr-Theppakorn@users.noreply.github.com>
2021-02-17 15:08:59 +0700

    add new file
    Color-Switcher

---
+++ JS-Color-Switcher/script.js
@@ -0,0 +1,112 @@
+document.getElementById('btn-Navy').onclick = switchNavy;
+document.getElementById('btn-Blue').onclick = switchBlue;
+document.getElementById('btn-Aqua').onclick = switchAqua;
+document.getElementById('btn-Teal').onclick = switchTeal;
+document.getElementById('btn-Olive').onclick = switchOlive;
+document.getElementById('btn-Green').onclick = switchGreen;
+document.getElementById('btn-Lime').onclick = switchLime;
+document.getElementById('btn-Yellow').onclick = switchYellow;
+document.getElementById('btn-Orange').onclick = switchOrange;
+document.getElementById('btn-Red').onclick = switchRed;
+document.getElementById('btn-Maroon').onclick = switchMaroon;
+document.getElementById('btn-Fuchsia').onclick = switchFuchsia;
+document.getElementById('btn-Purple').onclick = switchPurple;
+document.getElementById('btn-Silver').onclick = switchSilver;
+document.getElementById('btn-Gray').onclick = switchGray;
+document.getElementById('btn-Black').onclick = switchBlack;
+
+function switchNavy() {
+    document.getElementsByTagName('body')[0].style.background = "#001f3f";
+    document.getElementsByTagName('body')[0].style.color = "white";
+
+}
+
+function switchBlue() {
+    document.getElementsByTagName('body')[0].style.background = "#0074D9";
+    document.getElementsByTagName('body')[0].style.color = "white";
+
+}
+
+function switchAqua() {
+    document.getElementsByTagName('body')[0].style.background = "#7FDBFF";
+    document.getElementsByTagName('body')[0].style.color = "white";
+
+}
+
+function switchTeal() {
+    document.getElementsByTagName('body')[0].style.background = "#39CCCC";
+    document.getElementsByTagName('body')[0].style.color = "white";
+
+}
+
+function switchOlive() {
+    document.getElementsByTagName('body')[0].style.background = "#3D9970";
+    document.getElementsByTagName('body')[0].style.color = "white";
+
+}
+
+function switchGreen() {
+    document.getElementsByTagName('body')[0].style.background = "#2ECC40";
+    document.getElementsByTagName('body')[0].style.color = "white";
+
+}
+
+function switchLime() {
+    document.getElementsByTagName('body')[0].style.background = "#01FF70";
+    document.getElementsByTagName('body')[0].style.color = "white";
+
+}
+
+function switchYellow() {
+    document.getElementsByTagName('body')[0].style.background = "#FFDC00";
+    document.getElementsByTagName('body')[0].style.color = "white";
+
+}
+
+function switchOrange() {
+    document.getElementsByTagName('body')[0].style.background = "#FF851B";
+    document.getElementsByTagName('body')[0].style.color = "white";
+
+}
+
+function switchRed() {
+    document.getElementsByTagName('body')[0].style.background = "#FF4136";
+    document.getElementsByTagName('body')[0].style.color = "white";
+
+}
+
+function switchMaroon() {
+    document.getElementsByTagName('body')[0].style.background = "#85144b";
+    document.getElementsByTagName('body')[0].style.color = "white";
+
+}
+
+function switchFuchsia() {
+    document.getElementsByTagName('body')[0].style.background = "#F012BE";
+    document.getElementsByTagName('body')[0].style.color = "white";
+
+}
+
+function switchPurple() {
+    document.getElementsByTagName('body')[0].style.background = "#B10DC9";
+    document.getElementsByTagName('body')[0].style.color = "white";
+
+}
+
+function switchSilver() {
+    document.getElementsByTagName('body')[0].style.background = "#DDDDDD";
+    document.getElementsByTagName('body')[0].style.color = "Black";
+
+}
+
+function switchGray() {
+    document.getElementsByTagName('body')[0].style.background = "#AAAAAA";
+    document.getElementsByTagName('body')[0].style.color = "white";
+
+}
+
+function switchBlack() {
+    document.getElementsByTagName('body')[0].style.background = "#111111";
+    document.getElementsByTagName('body')[0].style.color = "white";
+
+}
